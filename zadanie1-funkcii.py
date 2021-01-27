e058dc9b70009c3973af36a9bd24d7d805a77f50
kozel86 <77636495+kozel86@users.noreply.github.com>
2021-01-27 15:23:03 +0300

    Add files via upload

---
+++ zadanie1-funkcii.py
@@ -0,0 +1,10 @@
+def squares(length, width, count=0):
+    if length < width:
+        length, width = width, length
+    length -= width
+    if length == 0:
+        print('квадрат номер: ', count + 1, ' с ребром ', width)
+        return 0
+    count += 1
+    print('квадрат номер: ', count, ' с ребром ', width)
+    return squares(length, width, count)
